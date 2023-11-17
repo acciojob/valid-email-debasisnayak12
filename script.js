bbc73f79d86d69cb4d49ff1095e95966dc83d4d8
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-17 19:11:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function validEmail(str) {
   //your JS code here.
+	//check if string is empty
+	if(str.includes(' ') || !str){
+		return false;
+	} 
+	const[localPart, domainPart] = str.split('@'); 
+ 
+	// check the localPart
+	if( !/[a-zA-Z0-9_-]/.test(localPart) ||
+	   localPart.includes('..') ||
+	   localPart.endsWith('.')) { 
+		return false;
+	   }    
+	//check the domainPart
+	if(!domainPart.includes('.') || domainPart.length < 3){
+		return false;
+	}
+	return true;	
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt("Enter an email address."); 
 alert(validEmail(str));
